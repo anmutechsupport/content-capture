ed77d8c1edf047a609e690ebfbcf17a550052a79
Anush M <69090449+anmutechsupport@users.noreply.github.com>
2021-06-28 18:52:47 -0400

    finally finally finally opencv works

--- video_parsing.py
+++ video_parsing.py
@@ -1,45 +1,23 @@
+from threading import local
 import cv2
 import tempfile
 import numpy as np
-
-# video = cv2.VideoCapture('test.mp4')
-
-# if (video.isOpened()== False):
-#   print("Error opening video stream or file")
-
-# num_frames = []
-# while video.isOpened():
-#     ret, frame = video.read()
-#     if ret:
-#         num_frames.append('go')
-#     # For testing purpose
-#         # num_frames.append(frame)
-#         # cv2.imshow("frame", frame)
-#         # if cv2.waitKey(25) == ord('q'):
-#         #     print("nice")
-#         #     break
-#     ##############################
-#         temp_file = tempfile.TemporaryFile()
-#         np.save(temp_file, frame)
-#         temp_file.seek(0)
-#         # upload_to_some_where(temp_file.read())
-#         temp_file.close()
-#     else:
-#         break
-
-# print(len(num_frames))
-
-# video.release()
-# cv2.destroyAllWindows()
+from PIL import Image
+import os
 
 """"""
 
-myFrameNumber = [250, 2000, 4000]
+myFrameNumber = [500, 2000, 4000]
 cap = cv2.VideoCapture("test.mp4")
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width  = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
 
 # get total number of frames
 totalFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 
+requestedFrames = []
 for frameInd in myFrameNumber:
 
 # check for valid frame number
@@ -47,22 +25,29 @@ for frameInd in myFrameNumber:
         # set frame position
         cap.set(cv2.CAP_PROP_POS_FRAMES,frameInd)
 
+    # requestedFrames = []
     numFrames = []
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
             numFrames.append(frame)
+
             # cv2.imshow("frame", frame)
             # if cv2.waitKey(25) == ord('q'):
             #     break
             if len(numFrames) == 25*10:
+                # print("haha")
+                requestedFrames.append(numFrames)
                 break 
-            #############################
-            temp_file = tempfile.TemporaryFile()
-            np.save(temp_file, frame)
-            temp_file.seek(0)
-            # upload_to_some_where(temp_file.read())
-            temp_file.close()
 
 cap.release()
+
+out = cv2.VideoWriter('video.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, (int(width), int(height)))
+for segment in requestedFrames:
+    for x in segment:
+        # writing to a image array
+        # print(x.shape)
+        out.write(x)
+out.release()
+
 cv2.destroyAllWindows()
